823cc3e89b347a2f9d2b2214910674cb93d79f44
Aris Liang <arisliang@navig8group.com>
2015-01-15 13:09:03 +0800

    adds more pages.

--- Mvc5AzureTest.MvcUI/Controllers/HomeController.cs
+++ Mvc5AzureTest.MvcUI/Controllers/HomeController.cs
@@ -14,5 +14,19 @@ namespace Mvc5AzureTest.MvcUI.Controllers
         {
             return View();
         }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your app description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
 	}
 }
